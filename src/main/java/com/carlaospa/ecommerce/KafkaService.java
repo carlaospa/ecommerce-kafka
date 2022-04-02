21d67eb4111b1860e9e845b5d63e68d766160d93
carlaospa <carlaospa@gmail.com>
2022-04-02 13:33:22 -0300

    Refatoramento de código

--- src/main/java/com/carlaospa/ecommerce/KafkaService.java
+++ src/main/java/com/carlaospa/ecommerce/KafkaService.java
@@ -7,16 +7,16 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class KafkaService {
-
 
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
-    KafkaService(String topic, ConsumerFunction parse) {
+    KafkaService(String groupId, String topic, ConsumerFunction parse) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties());
+        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
         consumer.subscribe(Collections.singletonList(topic));
     }
 
@@ -32,13 +32,14 @@ public class KafkaService {
         }
     }
 
-    private static Properties properties() {
+    private static Properties properties(String groupId) {
 
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         return properties;
     }
 }
