286d2a7c3091213cbfe5feb058064ee8bb9894a7
carlaospa <carlaospa@gmail.com>
2022-04-02 17:04:11 -0300

    Refatoramento de código

--- src/main/java/com/carlaospa/ecommerce/KafkaDispatcher.java
+++ src/main/java/com/carlaospa/ecommerce/KafkaDispatcher.java
@@ -10,16 +10,16 @@ import java.io.Closeable;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-class KafkaDispatcher implements Closeable {
+class KafkaDispatcher<T> implements Closeable {
 
-    private final KafkaProducer<String, String> producer;
+    private final KafkaProducer<String, T> producer;
 
     KafkaDispatcher(){
-        this.producer = new KafkaProducer<String, String>(properties());
+        this.producer = new KafkaProducer<>(properties());
     }
 
-    void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
-        var record = new ProducerRecord<String, String>(topic,key, value);
+    void send(String topic, String key, T value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<>(topic,key, value);
         producer.send(record, callback()).get();
     }
 
@@ -38,7 +38,7 @@ class KafkaDispatcher implements Closeable {
         var properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
         return properties;
     }
 
--- src/main/java/com/carlaospa/ecommerce/NewOrderMain.java
+++ src/main/java/com/carlaospa/ecommerce/NewOrderMain.java
@@ -1,19 +1,26 @@
 
 package com.carlaospa.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 20; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = "123456,65321,11.00";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 20; i++) {
 
-                var email = "Obrigado! Nós processamos seu pedido!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Obrigado! Nós processamos seu pedido!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
