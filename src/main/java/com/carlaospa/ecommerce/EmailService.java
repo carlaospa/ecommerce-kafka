cd3eb2c090337a6525f4f09b4fad04e61c3db1f1
carlaospa <carlaospa@gmail.com>
2022-04-03 11:37:49 -0300

    Refatoramento de código

--- src/main/java/com/carlaospa/ecommerce/EmailService.java
+++ src/main/java/com/carlaospa/ecommerce/EmailService.java
@@ -1,11 +1,15 @@
 package com.carlaospa.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+
 public class EmailService {
 
     public static void main(String[] args) {
         var emailService = new EmailService();
-        try( var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", emailService::parse)){
+        try(var service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL", emailService::parse, String.class, Map.of())){
             service.run();
         }
     }
--- src/main/java/com/carlaospa/ecommerce/FraudDetectorService.java
+++ src/main/java/com/carlaospa/ecommerce/FraudDetectorService.java
@@ -2,16 +2,19 @@ package com.carlaospa.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class FraudDetectorService {
 
     public static void main(String[] args) {
         var fraudService = new FraudDetectorService();
-        try( var service = new KafkaService(FraudDetectorService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER", fraudService::parse)) {
+        try(var service = new KafkaService(FraudDetectorService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER",
+                fraudService::parse, Order.class, Map.of())) {
             service.run();
         }
     }
 
-    private void parse(ConsumerRecord<String, String> record) {
+    private void parse(ConsumerRecord<String, Order> record) {
         System.out.println("-------------------------------------------");
         System.out.println("Processando nova ordem, checando fraudes ");
         System.out.println(record.key());
